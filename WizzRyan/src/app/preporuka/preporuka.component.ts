559bcc3fa18c03c73186e29fe723b189a872e610
Marija4400 <marija.milovanovic.4400@metropolitan.ac.rs>
2022-12-13 19:04:22 +0100

    IT255-DZ08

---
+++ WizzRyan/src/app/preporuka/preporuka.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-preporuka',
+  templateUrl: './preporuka.component.html',
+  styleUrls: ['./preporuka.component.css']
+})
+export class PreporukaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
