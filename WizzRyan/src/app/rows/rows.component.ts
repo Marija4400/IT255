d94bbacaf2be2ab6e04a5996c1e09257441f7f34
Marija4400 <marija.milovanovic.4400@metropolitan.ac.rs>
2022-12-13 23:39:31 +0100

    Domaci-08

--- WizzRyan/src/app/rows/rows.component.ts
+++ WizzRyan/src/app/rows/rows.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-rows',
@@ -10,13 +10,14 @@ export class RowsComponent implements OnInit {
 
   userForm: FormGroup;
   listData:any;
+  cena:number = 100;
 
   constructor(private fb: FormBuilder){
 
     this.listData=[];
 
     this.userForm = this.fb.group({
-      name :['',Validators.required],
+      name :['',Validators.required,Validators.minLength(6)],
       address:['',Validators.required],
       contactNo:['',Validators.required],
       gender:['',Validators.required]
@@ -40,6 +41,28 @@ export class RowsComponent implements OnInit {
       this.listData.splice(index,1);
       });
       
+  }
+
+  public uvecajCenu(){
+    this.cena=100;
+    const ch1:any = document.getElementById("ch1");
+    const ch2:any = document.getElementById("ch2");
+    const ch3:any= document.getElementById("ch3");
+
+    if(ch1.checked==true){
+      this.cena+=1000;
+
+    }
+    
+    if(ch2.checked==true){
+      this.cena+=500;
+      
+    }
+    
+    if(ch3.checked==true){
+      this.cena+=100;
+      
+    }
   }
 
 
